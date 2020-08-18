515ab2734dfde2e406927b114072fb52581874a8
jayeshkolpurath <67106845+jayeshkolpurath@users.noreply.github.com>
2020-08-18 20:21:49 +0530

    Sub Menu in Left Menu

--- SchoolManager/SchoolManager/Controllers/LayoutController.js
+++ SchoolManager/SchoolManager/Controllers/LayoutController.js
@@ -5,21 +5,24 @@ class LayoutController extends ControllerBase {
         super();
     }
     HandleMenuClicks() {
-        //Top Panel Menu Clicks
-        MenuItems = this._FetchTopMenuItems();
-        for (var i = 0; i < MenuItems.length; i++) {
-            var Nm = $("<div>", { id: "TopMenu" + (i + 1), class: "TopMenu ThemeTopMenu", text: MenuItems[i].MenuText });
-            Nm.on("click", { MenuItem: MenuItems[i] }, MenuClick);
-            $("#TopMenuPanel").append(Nm);
+        try {
+            //Top Panel Menu Clicks
+            MenuItems = this._FetchTopMenuItems();
+            for (var i = 0; i < MenuItems.length; i++) {
+                var Nm = $("<div>", { id: "TopMenu" + (i + 1), class: "TopMenu ThemeTopMenu", text: MenuItems[i].MenuText });
+                Nm.on("click", { MenuItem: MenuItems[i], Settings: JSON.parse(JSON.stringify(this.Settings)) }, MenuClick);
+                $("#TopMenuPanel").append(Nm);
+            }
+        } catch (Ex) {
+            new LogHelper(this.Settings).LogError("HandleMenuClicks", Ex);
         }
-        
     }
 
     _FetchTopMenuItems() {
         return [
-            { MenuName: "Home", MenuText: "Home", MenuLink: "/Views/Home.html" },
-            { MenuName: "Student", MenuText: "Student Register", MenuLink: "/Views/StudentRegister.html" },
-            { MenuName: "Department", MenuText: "Department", MenuLink: "/Views/Departments.html" }
+            { MenuName: "Home", MenuText: "Home" },
+            { MenuName: "Student", MenuText: "Student Register" },
+            { MenuName: "Settings", MenuText: "Settings" }
         ];
     }
 
@@ -28,8 +31,19 @@ class LayoutController extends ControllerBase {
         switch (TopMenu) {
             case "Home":
                 SideMenu = [
-                    { MenuName: "Home", MenuText: "Home", MenuLink: "/Views/Home.html" },
-                    { MenuName: "Gallery", MenuText: "Gallery", MenuLink: "/Views/Gallery.html" },
+                    {
+                        MenuName: "Home", MenuText: "Home", SubMenu: [
+                            { MenuName: "Home", MenuText: "Dashboard", MenuLink: "/Views/Home.html" },
+                            {
+                                MenuName: "Gallery", MenuText: "Gallery 4", SubMenu: [
+                                    { MenuName: "Gallery", MenuText: "Photos", MenuLink: "/Views/Gallery.html" },
+                                    { MenuName: "Gallery", MenuText: "Photos", MenuLink: "/Views/Gallery.html" },
+                                    { MenuName: "Gallery", MenuText: "Photos", MenuLink: "/Views/Gallery.html" },
+                                    { MenuName: "Gallery", MenuText: "Events", MenuLink: "/Views/Events.html" }
+                                ]
+                            }
+                        ]
+                    },
                     { MenuName: "Notifications", MenuText: "Notifications", MenuLink: "/Views/Notifications.html" }
                 ];
                 break;
@@ -39,8 +53,15 @@ class LayoutController extends ControllerBase {
                     { MenuName: "FeesCollection", MenuText: "Student Fees", MenuLink: "/Views/FeesCollection.html" },
                     { MenuName: "MarksEntry", MenuText: "Marks Entry", MenuLink: "/Views/MarksEntry.html" }];
                 break;
-            case "Department":
-                SideMenu = [{ MenuName: "Departments", MenuText: "Departments", MenuLink: "/Views/Departments.html" }];
+            case "Settings":
+                SideMenu = [
+                    {
+                        MenuName: "MasterData", MenuText: "Master Data", SubMenu: [
+                            { MenuName: "Departments", MenuText: "Departments", MenuLink: "/Views/Departments.html" },
+                            { MenuName: "Departments", MenuText: "Departments", MenuLink: "/Views/Departments.html" }
+                        ]
+                    }
+                ];
                 break;
         }
         return SideMenu;
@@ -51,19 +72,27 @@ class LayoutController extends ControllerBase {
     }
 
     GetTheme() {
-        var ThemeName;
-        var objStorage = new StorageHelper();
-        if (objStorage.Get("Theme")) {
-            ThemeName = objStorage.Get("Theme");
-        } else {
-            ThemeName = "Classic";
+        try {
+            var ThemeName;
+            var objStorage = new StorageHelper();
+            if (objStorage.Get("Theme")) {
+                ThemeName = objStorage.Get("Theme");
+            } else {
+                ThemeName = "Classic";
+            }
+        } catch (Ex) {
+            new LogHelper(this.Settings).LogError("GetTheme", Ex);
         }
         return ThemeName;
     }
 
     SetTheme(ThemeName) {
-        var objStorage = new StorageHelper();
-        objStorage.Set("Theme", ThemeName);
+        try {
+            var objStorage = new StorageHelper();
+            objStorage.Set("Theme", ThemeName);
+        } catch (Ex) {
+            new LogHelper(this.Settings).LogError("SetTheme", Ex);
+        }
     }
 }
 
@@ -72,94 +101,162 @@ class LayoutController extends ControllerBase {
 var LoadedScripts = [];
 var Ctrl;
 $(document).ready(function () {
-    Ctrl = new LayoutController();
-    Ctrl.PageInit();
-    Ctrl.HandleMenuClicks();
-    LoadTheme(Ctrl);
-    if (Ctrl.SessionCheck()) {
-        var objStorage = new StorageHelper();
-        var UserName = objStorage.Get("SACurrentUserName");
-        $("#UserName").html(UserName);
-        setTimeout("LoadContentPage('PagePanel', '/Views/Home.html', 'Home')",10);
-    } else {
-        $("#TitlePanel").css("display", "none");
-        Ctrl.Logout();
-        window.location = "/Views/Login.html";
-    }
+    try {
+        Ctrl = new LayoutController();
+        Ctrl.PageInit();
+        Ctrl.HandleMenuClicks();
+        LoadTheme(Ctrl);
+        if (Ctrl.SessionCheck()) {
+            var objStorage = new StorageHelper();
+            var UserName = objStorage.Get("SACurrentUserName");
+            $("#UserName").html(UserName);
+            setTimeout("LoadContentPage('PagePanel', '/Views/Home.html', 'Home')", 10);
+        } else {
+            $("#TitlePanel").css("display", "none");
+            Ctrl.Logout();
+            window.location = "/Views/Login.html";
+        }
 
-    $("#btnTheme").click(function () {
-        var Themes = Ctrl._FetchThemes();
-        var html = "<div id='lst_ThemeOptions'><div>Select Theme</div><ul>";
-        Themes.forEach(element => {
-            html += "<li onclick='ThemeSelect(\"" + element.Name + "\")'>" + element.Name + "</li>";
+        $("#btnTheme").click(function () {
+            var Themes = Ctrl._FetchThemes();
+            var html = "<div id='lst_ThemeOptions'><div>Select Theme</div><ul>";
+            Themes.forEach(element => {
+                html += "<li onclick='ThemeSelect(\"" + element.Name + "\")'>" + element.Name + "</li>";
+            });
+            html += "</ul></div>";
+            $("#PopupBox").html(html);
+            $("#PopupBox").show();
         });
-        html += "</ul></div>";
-        $("#PopupBox").html(html);
-        $("#PopupBox").show();
-    });
 
-    $("#lnkSignOut").click(function () {
-        Ctrl.Logout(); 
-        window.location = "/Views/Login.html";
-    });
-    //Menu Click Handling
+        $("#lnkSignOut").click(function () {
+            Ctrl.Logout();
+            window.location = "/Views/Login.html";
+        });
+        //Menu Click Handling
+    } catch (Ex) {
+        new LogHelper(Ctrl.Settings).LogError("doc ready", Ex);
+    }
 });
 
 function ThemeSelect(ThemeName) {
-    Ctrl.SetTheme(ThemeName);
-    $("#PopupBox").hide();
-    window.location = "/Views/Layout.html";
+    try {
+        Ctrl.SetTheme(ThemeName);
+        $("#PopupBox").hide();
+        window.location = "/Views/Layout.html";
+    } catch (Ex) {
+        new LogHelper(this.Settings).LogError("ThemeSelect", Ex);
+    }
 }
 var MenuItems;
 function MenuClick(e) {
-    $("#LeftPanel").empty();
-    $("#LeftPanel").hide();
-    var SubMenuList = Ctrl._FetchLeftMenuItems(e.data.MenuItem.MenuName);
-    for (var i = 0; i < SubMenuList.length; i++) {
-        var Nm = $("<div>", { id: "LeftMenu" + (i + 1), class: "LeftMenu ThemeLeftMenu", text: SubMenuList[i].MenuText });
-        Nm.on("click", { MenuItem: SubMenuList[i] }, SubMenuClick);
-        $("#LeftPanel").append(Nm);
+    try {
+        $("#LeftPanel").empty();
+        $("#LeftPanel").hide();
+        var SubMenuList = Ctrl._FetchLeftMenuItems(e.data.MenuItem.MenuName);
+        var i = 0;
+        SubMenuList.forEach(element => {
+            GenerateMenuItems(element, $("#LeftPanel"), i, 0, "_", e.data.Settings);
+            i++;
+        });
+
+        $("#LeftPanel").show();
+    } catch (Ex) {
+        new LogHelper(e.data.Settings).LogError("MenuClick", Ex);
     }
-    $("#LeftPanel").show();
+}
+
+function GenerateMenuItems(Element, Parent, Pos, Level, Suffix, Settings) {
+    try {
+        var Nm;
+        var DisplayStyle = (Parent[0].id == "LeftPanel") ? "display:block;" : "display:none;";
+        var eID = "LeftMenu" + Suffix + "_" + Level + "" + Pos;
+        if (typeof Element.SubMenu !== 'undefined') {
+            Nm = $("<div>", { id: eID, class: "LeftMenuHead ThemeLeftMenuHead ", style: DisplayStyle, text: Element.MenuText });
+            Nm.on("click", { MenuItem: Element, Settings: Settings, ID: eID  }, SubMenuClick);
+            var j = 1;
+            Element.SubMenu.forEach(elm => {
+                GenerateMenuItems(elm, Nm, j, Level+1, Suffix + "_" + Level + "" +Pos , Settings);
+                j++;
+            });
+            Parent.append(Nm);
+        }
+        else {
+            Nm = $("<div>", { id: "LeftMenu" + Suffix + "_" + Level + "" + Pos, class: "LeftMenu ThemeLeftMenu ", style: DisplayStyle, text: Element.MenuText });
+            Nm.on("click", { MenuItem: Element, Settings: Settings, ID: eID  }, SubMenuClick);
+            Parent.append(Nm);
+        }
+    }
+    catch (Ex) {
+        new LogHelper(Settings).LogError("GenerateMenuItems", Ex);
+    }
 }
 
 function SubMenuClick(e) {
-    var MenuItem = e.data.MenuItem;
-    LoadContentPage("PagePanel", MenuItem.MenuLink, MenuItem.MenuName);
+    try {
+        if (typeof e.data.MenuItem.SubMenu != 'undefined') {
+            var ID = e.data.ID;
+            $("#" + ID).children().each(function (index) {
+                var Item = $(this);
+                Item.toggle();
+            });
+        } else {
+            var MenuItem = e.data.MenuItem;
+            LoadContentPage("PagePanel", e.data.MenuItem.MenuLink, e.data.MenuItem.MenuName);
+        }
+    } catch (Ex) {
+        new LogHelper(e.data.Settings).LogError("SubMenuClick", Ex);
+    }
+    return false;
 }
 
 function LoadTheme(Ctrl) {
-    var ThemeName = Ctrl.GetTheme();
-    var ThemeUTL = "/Content/CSS/Themes/" + ThemeName + ".css";
-    var cssFile = document.createElement("link");
-    cssFile.rel = "stylesheet";
-    cssFile.Type = "text/css";
-    cssFile.href = ThemeUTL;
-    document.head.appendChild(cssFile);
+    try {
+        var ThemeName = Ctrl.GetTheme();
+        var ThemeUTL = "/Content/CSS/Themes/" + ThemeName + ".css";
+        var cssFile = document.createElement("link");
+        cssFile.rel = "stylesheet";
+        cssFile.Type = "text/css";
+        cssFile.href = ThemeUTL;
+        document.head.appendChild(cssFile);
+    } catch (Ex) {
+        new LogHelper(this.Settings).LogError("LoadTheme", Ex);
+    }
 }
 
 function LoadContentPage(Location, ContentHTMLPageURL, ControllerName) {
-    $("#" + Location).load(ContentHTMLPageURL);
-    LoadScripts(ControllerName);
-    LoadCSS(ControllerName);
-    var fn = "Render" + ControllerName + "Page()";
-    setTimeout(fn, 10);
+    try {
+        $("#" + Location).load(ContentHTMLPageURL);
+        LoadScripts(ControllerName);
+        LoadCSS(ControllerName);
+        var fn = "Render" + ControllerName + "Page()";
+        setTimeout(fn, 10);
+    } catch (Ex) {
+        new LogHelper(this.Settings).LogError("LoadContentPage", Ex);
+    }
 }
 
 function LoadScripts(ControllerName) {
-    var ScriptURLs = [{ Name: "Helper" }, { Name: "Model" }, { Name: "Controller" }];
-    ScriptURLs.forEach(element => {
-        var URL = "../" + element.Name + "s/" + ControllerName + element.Name + ".js";
-        if (!$("script[src='" + URL + "']").length) {
-            $('<script src="' + URL + '" type="text/javascript"></script>').appendTo("head");
-        }
-    });
+    try {
+        var ScriptURLs = [{ Name: "Helper" }, { Name: "Model" }, { Name: "Controller" }];
+        ScriptURLs.forEach(element => {
+            var URL = "../" + element.Name + "s/" + ControllerName + element.Name + ".js";
+            if (!$("script[src='" + URL + "']").length) {
+                $('<script src="' + URL + '" type="text/javascript"></script>').appendTo("head");
+            }
+        });
+    } catch (Ex) {
+        new LogHelper(this.Settings).LogError("LoadScripts", Ex);
+    }
 }
 
 function LoadCSS(ControllerName) {
-    var URL = "/Content/CSS/" + ControllerName + ".css";
-    if (!$("link[href='" + URL + "']").length) {
-        $('<link href="' + URL + '" rel="stylesheet">').appendTo("head");
+    try {
+        var URL = "/Content/CSS/" + ControllerName + ".css";
+        if (!$("link[href='" + URL + "']").length) {
+            $('<link href="' + URL + '" rel="stylesheet">').appendTo("head");
+        }
+    } catch (Ex) {
+        new LogHelper(this.Settings).LogError("LoadCSS", Ex);
     }
 }
 
